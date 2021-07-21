431fd73ff03504fc8ca718a1c6222d883e5ac544
Hannah <hannahwoodward@hotmail.com>
2021-07-21 14:57:46 +0800

    tested in replit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+console.log(multiply);
 
+let random = (Math.floor(Math.random()*10)+1);
+console.log(random);
+
+let num3 = 12;
+let num4 = 3
+
+let mod = (num3 / num4);
+console.log(mod);
+
+let max = Math.max(1, 5, 11, 17, 20);
+console.log(max);
 // Write your code here
